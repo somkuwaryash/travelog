5e2ad81251df1d705a54f1c783d5ac7a964212b3
yash somkuwar <somkuwar.techfest@gmail.com>
2023-08-26 07:17:56 +0530

    Initial Commit

---
+++ TravelLog/TravelLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLogApp.swift
+//  TravelLog
+//
+//  Created by Yash Somkuwar on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
